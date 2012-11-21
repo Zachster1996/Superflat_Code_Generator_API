877fe7faeadf7f8f115c95dffea1428f98f9c7a6
Zach Thompson <notmyemail@toobad.com>
2012-11-20 22:57:12 -0500

    Fixed A Problem With Assembly Information

--- C#/SuperflatCodeGeneratorAPI/Properties/AssemblyInfo.cs
+++ C#/SuperflatCodeGeneratorAPI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SuperflatCodeGeneratorAPI")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Zach Thompson 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
