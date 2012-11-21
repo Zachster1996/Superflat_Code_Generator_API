9b7017f01b5f8350df851749d99dddadad4755b7
Zach Thompson <notmyemail@toobad.com>
2012-11-20 22:49:23 -0500

    Uploaded Original Source

---
+++ Java/src/com/Zachster1996/SuperflatCodeGeneratorAPI/Code.java
@@ -0,0 +1,47 @@
+package com.Zachster1996.SuperflatCodeGeneratorAPI;
+
+public class Code {
+	public static String Generate(String[] layers, int biome, Boolean enableVillages, int villageSize, int villageDistance, Boolean enableMineshafts, double mineshaftChance, Boolean enableStrongholds, int strongholdCount, Boolean enableStructures, Boolean enableDungeons, Boolean enableDecorations, Boolean enableLakes, Boolean enableLavaLakes)
+    {
+        String code = "2;";
+        for (int i = 0; i < layers.length - 1; i++)
+        {
+            code += layers[i] + ",";
+        }
+        code += layers[layers.length - 1] + ";";
+        code += biome + ";";
+        if (enableVillages)
+        {
+            code += "village(size=" + villageSize + " distance=" + villageDistance + "),";
+        }
+        if (enableMineshafts)
+        {
+            code += "mineshaft(chance=" + mineshaftChance + "),";
+        }
+        if (enableStrongholds)
+        {
+            code += "stronghold(count=" + strongholdCount + "),";
+        }
+        if (enableStructures)
+        {
+            code += "biome_1,";
+        }
+        if (enableDungeons)
+        {
+            code += "dungeon";
+        }
+        if (enableDecorations)
+        {
+            code += "decoration,";
+        }
+        if (enableLakes)
+        {
+            code += "lake,";
+        }
+        if (enableLavaLakes)
+        {
+            code += "lava_lake,";
+        }
+        return code;
+    }
+}
